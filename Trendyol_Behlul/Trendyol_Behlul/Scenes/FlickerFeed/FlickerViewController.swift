a72acce50da241328cb3f94c4154ed3a5526271e
behlul <behlulkusaslan@gmail.com>
2019-01-08 17:48:52 +0300

    feed collection view connected to API.

--- Trendyol_Behlul/Trendyol_Behlul/Scenes/FlickerFeed/FlickerViewController.swift
+++ Trendyol_Behlul/Trendyol_Behlul/Scenes/FlickerFeed/FlickerViewController.swift
@@ -15,41 +15,89 @@ final class FlickerViewController: UIViewController {
     @IBOutlet weak var flickerView: FlickerView!
     
     // Variables
+    var loopCount = -1
+    var photos: [Photo] = []
+    var persons: [Person] = [] {
+        didSet {
+            var presentations: [FlickerViewPresentation] = []
+            for (index, person) in persons.enumerated() {
+                presentations.append(FlickerViewPresentation(photo: photos[index], person: person))
+            }
+            state = .ready(presentations)
+        }
+    }
     var service: ServiceProtocol!
+    var state = State.loading {
+        didSet {
+            switch state {
+            case .loading:
+                flickerView.setLoading(true)
+            case .ready(let feedObjects):
+                flickerView.setLoading(false)
+                flickerView.updateCollectionView(feedObjects)
+            case .error:
+                flickerView.setLoading(false)
+            }
+        }
+    }
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
         flickerView.delegate = self
         // TODO: fetch data from service and pass into view
+        fetcFeed()
         
-        // TODO: Test purposes remove from here
-        service.getRecentPosts { (result) in
+    }
+    
+    // Functions
+    private func fetcFeed() {
+        service.getRecentPosts { [weak self] (result) in
+            guard let strongSelf = self else { return }
             switch result {
             case .success(let value):
                 debugPrint(value)
+                strongSelf.photos = value.photos.photo
+                strongSelf.persons = []
+                for photo in value.photos.photo {
+                    strongSelf.getUser(with: photo.owner)
+                }
             case .failure(let error):
                 debugPrint(error.localizedDescription)
             }
         }
-        
-        service.getUserInfo(userId: "161523614@N06") { (result) in
+    }
+    
+    private func getUser(with userId: String) {
+        service.getUserInfo(userId: userId) { [weak self] (result) in
+            guard let strongSelf = self else { return }
             switch result {
             case .success(let value):
                 debugPrint(value)
+                strongSelf.persons.append(value.person)
             case .failure(let error):
                 debugPrint(error.localizedDescription)
             }
         }
-        
     }
     
 }
 
+// MARK: - FlickerViewDelegate
 extension FlickerViewController: FlickerViewDelegate {
     func didCellSelected(at: Int) {
         print(at)
         // TODO: Go to detail view with object
+    }
+}
+
+// MARK: - State
+extension FlickerViewController {
+    enum State {
+        case loading
+        case ready([FlickerViewPresentation])
+        case error
     }
 }
--- Trendyol_Behlul/Trendyol_Behlul/Scenes/FlickerFeed/FlickerViewPresentation.swift
+++ Trendyol_Behlul/Trendyol_Behlul/Scenes/FlickerFeed/FlickerViewPresentation.swift
@@ -7,10 +7,21 @@
 //
 
 import Foundation
+import struct NetworkAPI.Photo
+import struct NetworkAPI.Person
 
 struct FlickerViewPresentation {
     let profileImagePath: String
     let postImagePath: String
     let profileName: String
     let postDescription: String
+}
+
+extension FlickerViewPresentation {
+    init(photo: Photo, person: Person) {
+        let profileImagePath = "http://farm\(photo.farm).staticflickr.com/\(photo.server)/\(photo.id)_\(photo.secret).jpg"
+        let postImagePath = "http://farm\(person.iconfarm).staticflickr.com/\(person.iconserver)/buddyicons/\(person.nsid).jpg"
+        
+        self.init(profileImagePath: profileImagePath, postImagePath: postImagePath, profileName: person.username.content, postDescription: photo.title)
+    }
 }
